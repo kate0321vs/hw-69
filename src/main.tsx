862aa0e654443aed62dbe0d03d45478a061ca57c
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-02-28 08:22:03 +0600

    Создала и настроила проект

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from "react-router-dom";
+import {store} from "./app/store.ts";
+import {Provider} from "react-redux";
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+
+)
+
